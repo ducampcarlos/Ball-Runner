495290afbf85b434a99218439fd507c77350c60c
ducampcarlos <ducampcarlos@gmail.com>
2025-03-31 14:18:49 -0300

    Adjust: adjusted Inputs to use new Input System, and have functionality both for mobile build and desktop build.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,34 +1,50 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
+//<summary>
+//This script is responsible for the player's movement, supporting both Android and PC platforms.
+//</summary>
 public class PlayerController : MonoBehaviour
 {
-    //<summary>
-    //This script is responsible for the player's movement for the Android platform.
-    //</summary>
-
     Rigidbody rb;
     [SerializeField] float jumpForce = 10f;
-    [SerializeField] bool canJump =true;
+    [SerializeField] bool canJump = true;
 
     private void Awake()
     {
-        rb=GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && canJump)
+        // Detects if the player is on Android platform
+#if UNITY_ANDROID
+        if (Touchscreen.current.primaryTouch.press.wasPressedThisFrame && canJump)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            canJump = false;
+            Jump();
         }
+#endif
 
-        //Gravity applied by code
+        // Detects if the player is on PC platform
+#if UNITY_STANDALONE
+        if (Mouse.current.leftButton.isPressed && canJump)
+        {
+            Jump();
+        }
+#endif
+
+        // Gravity applied by code
         rb.AddForce(Vector3.down * 20f, ForceMode.Force);
     }
 
-    //When the player collides with the ground, the player can jump again.
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        canJump = false;
+    }
+
+    // When the player collides with the ground, the player can jump again.
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
