f14446bd74c898f12bcf7de7a6a50050b31ca717
ducampcarlos <ducampcarlos@gmail.com>
2025-03-31 13:51:49 -0300

    Adjust: Game Start.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Obstacle Spawning")]
     [SerializeField] GameObject obstacle;
     [SerializeField] Transform spawnPoint;
     [SerializeField] float minSpawnTime = 0.5f;
     [SerializeField] float maxSpawnTime = 2f;
     Coroutine spawnObstacles;
+
+    [Header("Score")]
+    [SerializeField] TextMeshProUGUI scoreText;
     int score = 0;
+
+    [Header("Game Objects")]
+    [SerializeField] GameObject playbutton;
+    [SerializeField] GameObject player;
+
+    private void Awake()
+    {
+        playbutton.SetActive(true);
+        player.SetActive(false);
+    }
 
     IEnumerator SpawnObstacles()
     {
@@ -19,8 +34,16 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void ScoreUp()
+    {
+        scure++;
+        scoreText.text = score.ToString();
+    }
+
     public void GameStart()
     {
+        player.SetActive(true);
+        playbutton.SetActive(false);
         spawnObstacles = StartCoroutine(SpawnObstacles());
     }
 }
