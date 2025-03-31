dcaba17238f5a81298eba041e13d2e44da10254e
ducampcarlos <ducampcarlos@gmail.com>
2025-03-31 13:22:06 -0300

    Added: Script for Player Control.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //<summary>
+    //This script is responsible for the player's movement for the Android platform.
+    //</summary>
+
+    Rigidbody rb;
+    [SerializeField] float jumpForce = 10f;
+    [SerializeField] bool canJump =true;
+
+    private void Awake()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && canJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            canJump = false;
+        }
+
+        //Gravity applied by code
+        rb.AddForce(Vector3.down * 20f, ForceMode.Force);
+    }
+
+    //When the player collides with the ground, the player can jump again.
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
+    }
+}
