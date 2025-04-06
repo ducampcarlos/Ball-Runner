90f1d0d6de248b146a3d923e3960359bf9e620fa
ducampcarlos <ducampcarlos@gmail.com>
2025-04-06 12:21:41 -0300

    Adjust: Rotation for the ship.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
     [SerializeField] float jumpForce = 10f;
     [SerializeField] bool canJump = true;
+    [SerializeField] GameObject shipObject;
 
     private void Awake()
     {
@@ -36,6 +37,30 @@ public class PlayerController : MonoBehaviour
 
         // Gravity applied by code
         rb.AddForce(Vector3.down * 20f, ForceMode.Force);
+
+        // Rotate ship when is going up to look up, and down when is going down
+        Quaternion targetRotation = Quaternion.identity;
+
+        if (rb.linearVelocity.y > 0)
+        {
+            targetRotation = Quaternion.Euler(-30, 0, 0);
+        }
+        else if (rb.linearVelocity.y < 0)
+        {
+            targetRotation = Quaternion.Euler(30, 0, 0);
+        }
+        else
+        {
+            targetRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        // Suavizar la rotación
+        float rotationSpeed = 10f; // Podés ajustar esto para más o menos suavidad
+        shipObject.transform.rotation = Quaternion.Lerp(
+            shipObject.transform.rotation,
+            targetRotation,
+            Time.deltaTime * rotationSpeed
+        );
     }
 
     private void Jump()
